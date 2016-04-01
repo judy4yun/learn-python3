20bd9eedf5d5f1f328e9063c69e62d3622ed02f3
kiruno <153470420@qq.com>
2016-04-01 11:09:29 +0800

    Update do_listcompr.py

--- samples/advance/do_listcompr.py
+++ samples/advance/do_listcompr.py
@@ -10,3 +10,7 @@ print([k + '=' + v for k, v in d.items()])
 
 L = ['Hello', 'World', 'IBM', 'Apple']
 print([s.lower() for s in L])
+
+#list中所有的字符串变成小写,排除非字符串元素
+m = ['Hello', 'World', 18, 'Apple', None]
+print([i.lower() for i in m if isinstance(i,str)])
