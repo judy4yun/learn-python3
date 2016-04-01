b4da86e26c453290424937bd2f1ddb0da1a503bf
kiruno <153470420@qq.com>
2016-04-01 11:54:55 +0800

    Update do_generator.py

--- samples/advance/do_generator.py
+++ samples/advance/do_generator.py
@@ -29,3 +29,22 @@ while 1:
         print('Generator return value:', e.value)
         break
 
+#杨辉三角
+# (代码作者的id：风袭6729) 
+def triangles():
+    a = [1]
+    while True:
+        yield a
+        a = [sum(i) for i in zip([0] + a, a + [0])]
+#通过观察杨辉三角可知，下一行的每一个元素都依次由本行中每两个相邻元素之和得到，这个方法可以用一个技巧实现，
+#即：将本行list拷贝出两个副本，将两个副本错1位，然后加在一起。
+#由于错位后，前后各多了一个元素，所以要在错位后的两个list的前后各加一个[0]来补齐（其实，这个0是理所当然的，是杨辉三角的一部分）。
+
+
+#输出部分代码
+n = 0
+for t in triangles():
+    print(t)
+    n = n + 1
+    if n == 10:
+        break
