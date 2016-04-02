b0e90fd46df311920d127e81c394a7e774970d02
kiruno <153470420@qq.com>
2016-04-02 13:32:03 +0800

    Update use_slots.py

--- samples/oop_advance/use_slots.py
+++ samples/oop_advance/use_slots.py
@@ -51,15 +51,15 @@ from types import MethodType
 
 Stu.set_age=MethodType(set_age,Stu)
 
-a.set_age(15) \\通过set_age方法，设置的类属性age的值
-s.set_age(11) \\也是设置类属性age的值，并把上个值覆盖掉
-print(s.age,a.age) \\由于a和s自身没有age属性，所以打印的是类属性age的值
+a.set_age(15) #\\通过set_age方法，设置的类属性age的值
+s.set_age(11) #\\也是设置类属性age的值，并把上个值覆盖掉
+print(s.age,a.age) #\\由于a和s自身没有age属性，所以打印的是类属性age的值
 
-a.age = 10  \\给实例a添加一个属性age并赋值为10
-s.age = 20  \\给实例b添加一个属性age并赋值为20
-\\这两个分别是实例a和s自身的属性，仅仅是与类属性age同名，并没有任何关系
+a.age = 10  #\\给实例a添加一个属性age并赋值为10
+s.age = 20  #\\给实例b添加一个属性age并赋值为20
+#\\这两个分别是实例a和s自身的属性，仅仅是与类属性age同名，并没有任何关系
 
-print(s.age,a.age)  \\打印的是a和s自身的age属性值，不是类age属性值
+print(s.age,a.age)  #\\打印的是a和s自身的age属性值，不是类age属性值
 
 #所以， 1，slots并不能严格限制属性的添加，可通过在方法里定义限制之外的属性来添加本不能添加的属性（当然，前提是方法没有被限制） 
 #2，类属性是公共属性，所有实例都可以引用的，前提是实例自身没有同名的属性，因此类属性不能随意更改（别的实例可能在引用类属性的值
